cb53070bf7857dddfd2354e5715a2a85ac1ba1fa
Bartłomiej Uroda <you@example.com>
2020-10-26 23:34:26 +0100

    nav animation in js

--- .history/js/main_20201026224916.js
+++ .history/js/main_20201026224916.js
@@ -5,13 +5,4 @@ const navLinks = document.querySelectorAll(".nav-link");
 selectElement(".burger-menu-icon").addEventListener("click", () => {
     selectElement(".nav-list").classList.toggle("active");
     selectElement(".burger-menu-icon").classList.toggle("toggle")
-
-    navLinks.forEach((link, index) => {
-        if (link.style.animation){
-            link.style.animation = ""
-        }else{
-            link.style.animation = `navLinkAnimate 0.5s ease forwards ${ index/7 + 0.5}s`
-            console.log(index/7 + 0.5)
-        }
-    })
 });
