0e8f5d22282f572c1bc8a2aec2523798342aae62
Bartłomiej Uroda <uroda127@gmail.com>
2023-04-15 13:58:32 +0200

    Internet rzeczy

--- js/main.js
+++ js/main.js
@@ -1,33 +1,38 @@
+//Bartek Uroda
 //navbar animation etc
 const selectElement = (s) => document.querySelector(s);
 const navLinks = document.querySelectorAll(".nav-link");
 
 selectElement(".burger-menu-icon").addEventListener("click", () => {
-    selectElement(".nav-list").classList.toggle("active");
-    selectElement(".burger-menu-icon").classList.toggle("toggle")
+	selectElement(".nav-list").classList.toggle("active");
+	selectElement(".burger-menu-icon").classList.toggle("toggle");
 
-    navLinks.forEach((link, index) => {
-        if (link.style.animation){
-            link.style.animation = ""
-        }else{
-            link.style.animation = `navLinkAnimate 0.5s ease forwards ${ index/7 + 0.5}s`
-            console.log(index/7 + 0.5)
-        }
-    })
+	navLinks.forEach((link, index) => {
+		if (link.style.animation) {
+			link.style.animation = "";
+		} else {
+			link.style.animation = `navLinkAnimate 0.5s ease forwards ${
+				index / 7 + 0.5
+			}s`;
+			console.log(index / 7 + 0.5);
+		}
+	});
 });
 //navbar hiding after choose section
-navLinks.forEach(link => {
-    link.addEventListener("click", () => {
-        selectElement(".nav-list").classList.toggle("active");
-        selectElement(".burger-menu-icon").classList.toggle("toggle");
+navLinks.forEach((link) => {
+	link.addEventListener("click", () => {
+		selectElement(".nav-list").classList.toggle("active");
+		selectElement(".burger-menu-icon").classList.toggle("toggle");
 
-        navLinks.forEach((link, index) => {
-            if (link.style.animation){
-                link.style.animation = ""
-            }else{
-                link.style.animation = `navLinkAnimate 0.5s ease forwards ${ index/7 + 0.5}s`
-                console.log(index/7 + 0.5)
-            }
-        })
-    })
-})
+		navLinks.forEach((link, index) => {
+			if (link.style.animation) {
+				link.style.animation = "";
+			} else {
+				link.style.animation = `navLinkAnimate 0.5s ease forwards ${
+					index / 7 + 0.5
+				}s`;
+				console.log(index / 7 + 0.5);
+			}
+		});
+	});
+});
